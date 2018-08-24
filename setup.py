8dcffc9533ed23b3335ee2fa08b453b6aebb69f8
netzkontrast <info@netzkontrst.com>
2018-08-24 10:39:53 +0200

    added cython

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
 from setuptools import find_packages
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+cmdclass = { }
+ext_modules = [ ]
+
+ext_modules += [
+    Extension("genetic_selection.GeneticSelectionCV", ["genetic_selection/__init__.pyx"]),
+]
+cmdclass.update({ 'build_ext': build_ext })
 
 setup(name='sklearn-genetic',
-      version='0.1.1',
+      version='0.1.2',
       description='Genetic feature selection module for scikit-learn',
-      url='http://github.com/manuel-calzolari/sklearn-genetic',
-      author='Manuel Calzolari',
+      url='http://github.com/netzkontrast/sklearn-genetic',
+      original_author='Manuel Calzolari',
+      author='Michael Schimmer',
       author_email='',
       license='GPLv3',
-      install_requires=['scikit-learn>=0.18', 'deap>=1.0.2'],
+      cmdclass=cmdclass,
+      ext_modules=ext_modules,
+      install_requires=['scikit-learn>=0.18', 'deap>=1.0.2', 'joblib'],
       packages=find_packages())
